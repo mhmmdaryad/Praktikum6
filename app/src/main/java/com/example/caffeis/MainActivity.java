5b0a110f67f991302e0bae20682a6db4a56573b1
Rizkiaputri <rizkiapute@gmail.com>
2020-06-12 17:17:44 +0700

    menampilkan signup dan login

--- app/src/main/java/com/example/caffeis/MainActivity.java
+++ app/src/main/java/com/example/caffeis/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.caffeis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+    public void daftar(View view) {
+        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
+    public void login (View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
+
