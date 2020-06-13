a206ce51a865c9d53c8111e490b64db16fd88e1b
Arya Dewangga <aryadewangga08@gmail.com>
2020-06-13 18:19:59 +0700

    transfer data detail

--- app/src/main/java/com/example/caffeis/MainActivity.java
+++ app/src/main/java/com/example/caffeis/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference foodlist;
+
+    int[] images={R.drawable.kopisusu,R.drawable.kentanggoreng};
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -34,53 +38,65 @@ public class MainActivity extends AppCompatActivity {
         btnroti = (LinearLayout) findViewById(R.id.roti);
         btnmartabak = (LinearLayout) findViewById(R.id.martabak);
         btnboba = (LinearLayout) findViewById(R.id.boba);
-        lokasi = (Button)findViewById(R.id.button4);
+
+        lokasi = (Button) findViewById(R.id.button4);
 
         lokasi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                        Intent MapsActivity = new Intent(MainActivity.this, MapsActivity.class);
-                        startActivity(MapsActivity);
-                    }
-                });
+                Intent MapsActivity = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(MapsActivity);
+            }
+        });
+
         btnkopi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                foodDetail.putExtra("Nama", "Es Kopi Susu");
+                foodDetail.putExtra("Harga", "15000");
+                foodDetail.putExtra("Foto", images[0]);
                 startActivity(foodDetail);
-                foodDetail.putExtra("MenuId", 1);
             }
         });
         btnkentang.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                foodDetail.putExtra("Nama", "Kentang Goreng");
+                foodDetail.putExtra("Harga", "20000");
+                foodDetail.putExtra("Foto", images[1]);
                 startActivity(foodDetail);
-                foodDetail.putExtra("MenuId", 2);
             }
         });
         btnroti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                foodDetail.putExtra("Nama", "Roti Bakar");
+                foodDetail.putExtra("Harga", "12000");
+                foodDetail.putExtra("Foto", "Es Kopi Susu");
                 startActivity(foodDetail);
-                foodDetail.putExtra("MenuId", 3);
             }
         });
         btnmartabak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                foodDetail.putExtra("Nama", "Martabak");
+                foodDetail.putExtra("Harga", "25000");
+                foodDetail.putExtra("Foto", "Es Kopi Susu");
                 startActivity(foodDetail);
-                foodDetail.putExtra("MenuId", 4);
             }
         });
         btnboba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                foodDetail.putExtra("Nama", "Es Boba");
+                foodDetail.putExtra("Harga", "22000");
+                foodDetail.putExtra("Foto", "Es Kopi Susu");
                 startActivity(foodDetail);
-                foodDetail.putExtra("MenuId", 5);
             }
         });
 
