5424cae98765d6827346e6a086fca3f95ba9f022
Arya Dewangga <aryadewangga08@gmail.com>
2020-06-13 12:56:46 +0700

    fooddetail back

--- app/src/main/java/com/example/caffeis/FoodDetail.java
+++ app/src/main/java/com/example/caffeis/FoodDetail.java
@@ -1,14 +1,75 @@
 package com.example.caffeis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import model.Food;
 
 public class FoodDetail extends AppCompatActivity {
+    TextView food_name, food_price;
+    ImageView food_image;
+    ElegantNumberButton numberButton;
+    Button btnSubmit;
+
+    String foodId="";
+    FirebaseDatabase database;
+    DatabaseReference foods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_detail);
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        foods= database.getReference("Foods");
+
+        //init view
+
+        numberButton= (ElegantNumberButton) findViewById(R.id.jumlah_menu);
+        btnSubmit= (Button)findViewById(R.id.buttonpesan);
+
+        food_name=(TextView)findViewById(R.id.nama_menu);
+        food_price=(TextView)findViewById(R.id.textharga);
+        food_image=(ImageView)findViewById(R.id.image_menu);
+
+        if (getIntent()!= null){
+            foodId=getIntent().getStringExtra("FoodId");
+            if(!foodId.isEmpty()){
+                getDetailFood(foodId);
+            }
+        }
+    }
+
+    private void getDetailFood(String foodId) {
+        foods.child(foodId);
+        foods.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Food food = dataSnapshot.getValue(Food.class);
+
+                Picasso.get().load(food.getImage()).into(food_image);
+                food_price.setText(food.getPrice());
+                food_name.setText(food.getName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/caffeis/MainActivity.java
+++ app/src/main/java/com/example/caffeis/MainActivity.java
@@ -2,16 +2,70 @@ package com.example.caffeis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class  MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+    int kopi = 1, kentang = 2, roti = 3, martabak = 4, boba = 5;
+    Button btnkopi, btnkentang, btnroti, btnmartabak, btnboba;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_main);
+
+        btnkopi = (Button) findViewById(R.id.coffe);
+        btnkentang = (Button) findViewById(R.id.kentang);
+        btnroti = (Button) findViewById(R.id.roti);
+        btnmartabak = (Button) findViewById(R.id.martabak);
+        btnboba = (Button) findViewById(R.id.boba);
+
+        btnkopi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                startActivity(foodDetail);
+                foodDetail.putExtra("FoodId", kopi);
+            }
+        });
+        btnkentang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                startActivity(foodDetail);
+                foodDetail.putExtra("FoodId", kentang);
+            }
+        });
+        btnroti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                startActivity(foodDetail);
+                foodDetail.putExtra("FoodId", roti);
+            }
+        });
+        btnmartabak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                startActivity(foodDetail);
+                foodDetail.putExtra("FoodId", martabak);
+            }
+        });
+        btnboba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent foodDetail = new Intent(MainActivity.this, FoodDetail.class);
+                startActivity(foodDetail);
+                foodDetail.putExtra("FoodId", boba);
+            }
+        });
+
     }
+
 }
 
